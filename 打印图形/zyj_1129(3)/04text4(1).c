7f026a872786d46ace6a21bd99be9276f9e49496
497863419 <497863419@qq.com>
2019-01-03 23:07:02 +0800

    打印*图案

---
+++ 打印图形/zyj_1129(3)/04text4(1).c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<windows.h>
+int main()
+{
+	int i, j;
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = 0; j < 2 * i + 1; j++)
+			printf("*");
+			printf("\n");
+	}
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = 0; j <2*(7-i)-1; j++)
+		{
+			printf("*");
+		}
+		  printf("\n");
+	}
+	system("pause");
+	return 0;
+}
